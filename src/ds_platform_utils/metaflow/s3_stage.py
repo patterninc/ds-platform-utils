cff9710a787bf3d60e7a530b303da2aa692952bd
Abhishek Patil <129820441+abhk2@users.noreply.github.com>
2026-02-18 11:16:34 +0530

    fix: comment out unused SQL generation code in S3 to Snowflake copy query

--- src/ds_platform_utils/metaflow/s3_stage.py
+++ src/ds_platform_utils/metaflow/s3_stage.py
@@ -102,13 +102,13 @@ def _generate_s3_to_snowflake_copy_query(  # noqa: PLR0913
     if not auto_create_table and overwrite:
         sql_statements.append(f"TRUNCATE TABLE IF EXISTS {table_name};")
 
-    columns_str = ",\n  ".join([f"PARSE_JSON($1):{col_name}::{col_type}" for col_name, col_type in table_defination])
+    # columns_str = ",\n  ".join([f"PARSE_JSON($1):{col_name}::{col_type}" for col_name, col_type in table_defination])
 
-    copy_query = f"""COPY INTO {table_name} FROM '@{snowflake_stage_path}'
-        FILE_FORMAT = (TYPE = 'parquet' USE_LOGICAL_TYPE = {use_logical_type})
-        MATCH_BY_COLUMN_NAME = 'CASE_INSENSITIVE'
-        ;"""
-    sql_statements.append(copy_query)
+    # copy_query = f"""COPY INTO {table_name} FROM '@{snowflake_stage_path}'
+    #     FILE_FORMAT = (TYPE = 'parquet' USE_LOGICAL_TYPE = {use_logical_type})
+    #     MATCH_BY_COLUMN_NAME = 'CASE_INSENSITIVE'
+    #     ;"""
+    # sql_statements.append(copy_query)
 
     # Combine all statements into a single SQL script
     return "\n\n".join(sql_statements)
