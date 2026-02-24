a9793b4c02c54d15c8350d9cc414f46fda0ccc1e
Abhishek Patil <129820441+abhk2@users.noreply.github.com>
2026-02-24 13:26:25 +0530

    fix: update S3 DataFrame retrieval to remove dtype_backend and improve file handling

--- src/ds_platform_utils/metaflow/s3.py
+++ src/ds_platform_utils/metaflow/s3.py
@@ -22,16 +22,18 @@ def _get_df_from_s3_file(path: str) -> pd.DataFrame:
         raise ValueError("Invalid S3 URI. Must start with 's3://'.")
 
     with _get_metaflow_s3_client() as s3:
-        return pd.read_parquet(s3.get(path).path, dtype_backend="pyarrow")
+        return pd.read_parquet(s3.get(path).path)
 
 
 def _get_df_from_s3_files(paths: list[str]) -> pd.DataFrame:
     if any(not path.startswith("s3://") for path in paths):
         raise ValueError("Invalid S3 URI. All paths must start with 's3://'.")
 
+    dfs = []
     with _get_metaflow_s3_client() as s3:
-        df_paths = [obj.path for obj in s3.get_many(paths)]
-        return pd.read_parquet(df_paths, dtype_backend="pyarrow")
+        for obj in s3.get_many(paths):
+            dfs.append(pd.read_parquet(obj.path))
+    return pd.concat(dfs, ignore_index=True)
 
 
 def _get_df_from_s3_folder(path: str) -> pd.DataFrame:
